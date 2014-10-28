efc19b6877e151c971df06f0ee23e04d77426ab6
Alexis <proxx.sylvanar@gmail.com>
2014-10-28 22:34:16 +0000

    Main

---
+++ src/CrackzNovitch/InventoryClean/Main.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace CrackzNovitch\InventoryClean;
+
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\item\Item;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\EventExecutor;
+use pocketmine\event\EventPriority;
+use pocketmine\event\Listener;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+
+// TODO code Here
